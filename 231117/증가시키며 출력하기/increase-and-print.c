b8c0e3efae9b2cecb50aad46f7b6484160350f3d
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-17 14:21:52 +0900

    Title: 증가시키며 출력하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 231117/증가시키며 출력하기/increase-and-print.c
+++ 231117/증가시키며 출력하기/increase-and-print.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    for(int i = 5; i < 18; i += 2){
+    for(int i = 5; i <= 17; i+= 2){
         printf("%d ", i);
     }
     return 0;
