a179079e9929004d4fa5186d073a5caabb845017
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-17 12:40:00 +0900

    Title: 자연수면 출력, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231117/자연수면 출력/output-only-natural-number.c
+++ 231117/자연수면 출력/output-only-natural-number.c
@@ -3,15 +3,15 @@
 int main() {
     int a, b;
     scanf("%d %d", &a, &b);
-    if(a >= 0){
-        for(int i = 0; i < b; i++){
+    
+    if (a >= 0) {
+        for (int i = 0; i < b; i++) {
             printf("%d", a);
         }
         printf("\n");
+    } else if (a == 0) {
+        printf("0\n");
     }
-    else{
-        printf("0");
-    }
-    return 0;
 
+    return 0;
 }
