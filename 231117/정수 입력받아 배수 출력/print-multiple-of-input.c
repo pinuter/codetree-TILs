5e67892aa6655048af9952b9ad79ff55649a46b2
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-17 14:25:54 +0900

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231117/정수 입력받아 배수 출력/print-multiple-of-input.c
+++ 231117/정수 입력받아 배수 출력/print-multiple-of-input.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int n, a;
+    int n;
     scanf("%d", &n);
-    a % n == 0;
-    for(int i = n; i <= 5 * n; i += n){
+    
+    for(int i = n; i <= 5 * n; i+=n){
         printf("%d ", i);
     }
     return 0;
