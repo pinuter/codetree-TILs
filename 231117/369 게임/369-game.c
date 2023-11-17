aca2c9c6c2916ae919409cb22bd5427ba7cb3be6
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-17 13:14:57 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231117/369 게임/369-game.c
+++ 231117/369 게임/369-game.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    // 변수 선언
     int n;
-    scanf("%d", &n);
-    for(int i = 1; i <= n; i++){
-        if(i % 3 == 0){
-            printf("0 ");
-        }
-        else if(i / 10 == 3 || i % 10 == 6 || i % 9);{
-            printf("0 ");
-        }
-        else if(i % 10 == 3 || i / 10 == 6 || i / 9){
-            printf("0 ");
-        }
-        else{
-        printf("%d ", i);}
+
+	// 입력
+	scanf("%d", &n);
+    
+    // 출력
+	for(int i = 1; i <= n; i++) {
+		if(i % 3 == 0)
+			printf("0 ");
+		else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+			printf("0 ");
+		else if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+			printf("0 ");
+		else
+			printf("%d ", i);
     }
+  
     return 0;
 }
