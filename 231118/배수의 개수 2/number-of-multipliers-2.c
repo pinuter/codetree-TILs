386d92d0b90830b5def8b89eb1cf1ec1981677fe
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-18 17:40:33 +0900

    Title: 배수의 개수 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231118/배수의 개수 2/number-of-multipliers-2.c
+++ 231118/배수의 개수 2/number-of-multipliers-2.c
@@ -3,12 +3,12 @@
 int main() {
     int n, cnt = 0;
     
-    for(int i = n; i <= n; i++){
+    for(int i = 1; i <= n; i++){
         scanf("%d", &n);
-        if(i % 2 == 1){
+        if(i % 2 == 1)
             cnt++;
         
-        }
+        
         
     }
     printf("%d", cnt);
