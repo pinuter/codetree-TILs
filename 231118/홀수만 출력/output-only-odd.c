f300574b1dcf919217356c7249692f7831318ad2
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-18 09:11:24 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231118/홀수만 출력/output-only-odd.c
+++ 231118/홀수만 출력/output-only-odd.c
@@ -2,7 +2,11 @@
 
 int main() {
     int a, b;
-    scanf("%d %d", a, b);
-    for(int i = a; )
+    scanf("%d %d", &a, &b);
+    for(int i = a; i <= b; i++){
+        if(i%2==1){
+            printf("%d ", i);
+        }
+    }
     return 0;
 }
