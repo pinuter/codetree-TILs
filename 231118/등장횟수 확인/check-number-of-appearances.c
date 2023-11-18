1020059083ae5c4ef01df5e43ecba38db617b96d
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-18 18:23:53 +0900

    Title: 등장횟수 확인, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231118/등장횟수 확인/check-number-of-appearances.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int cnt2 = 0;
+    for(int i = 0; i <= 5; i++){
+        scanf("%d", &a);
+        if(a%2==0){
+            
+            cnt2++;
+        }
+    }
+    printf("%d", cnt2);
+    return 0;
+}
