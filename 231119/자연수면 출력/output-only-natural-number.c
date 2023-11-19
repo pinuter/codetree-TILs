eeb003e14fdc03723a9938023107638740e8a236
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-19 13:05:48 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231119/자연수면 출력/output-only-natural-number.c
+++ 231119/자연수면 출력/output-only-natural-number.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    // 변수 선언
     int a, b;
-
-	// 입력
-	scanf("%d %d", &a, &b);
-    
-    // 출력
-	if(a >= 1) {
-		for(int i = 1; i <= b; i++)
-			printf("%d", a);
-	}
-	else {
-		printf("0");
-	}
-	
+    scanf("%d %d", &a, &b);
+    if(a >= 1){
+        for(int i = 1; i <= b; i++){
+            printf("%d", a);
+        }
+    }
+    else{
+        printf("0");
+    }
     return 0;
 }
