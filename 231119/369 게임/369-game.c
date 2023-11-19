96689f0e8f5507f171abf71eb5322b1fc102c630
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-19 16:59:56 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231119/369 게임/369-game.c
+++ 231119/369 게임/369-game.c
@@ -4,7 +4,7 @@ int main() {
     int n;
     scanf("%d", &n);
     for(int i = 1; i <= n; i++){
-        if(i % 3 == 0 || i % 10 == 3){
+        if(i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9){
             printf("0");
         }
         else{
