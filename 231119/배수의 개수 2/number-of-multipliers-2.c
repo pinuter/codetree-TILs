8bacedb1cd028906b1ca07008882a33908f9754d
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-19 17:16:14 +0900

    Title: 배수의 개수 2, Time: 1092ms, Memory: 80MB, Status: Time Limit Exceed - CodeTree

---
+++ 231119/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int cnt = 0;
+    for(int i = 1; i <= 10, i++;){
+        if(i % 2 == 1){
+            cnt++;
+        }
+        printf("%d", cnt);
+    }
+    return 0;
+}
