5a8e6ae7f27fbc730c0e87f1e86ad392a71b8d7f
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-19 22:51:52 +0900

    Title: 정상적인 구구단, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231119/정상적인 구구단/normal-multiple.c
+++ 231119/정상적인 구구단/normal-multiple.c
@@ -6,7 +6,7 @@ int main() {
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
             printf("%d * %d = %d", i, j, i * j);
-            if(j < 3) printf(", ");
+            if(j < n) printf(", ");
         }
         printf("\n"); 
     }
