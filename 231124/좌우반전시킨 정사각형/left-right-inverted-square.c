a472db96cbba094262bbfe106b18e94623546d84
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-24 12:56:44 +0900

    Title: 좌우반전시킨 정사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231124/좌우반전시킨 정사각형/left-right-inverted-square.c
+++ 231124/좌우반전시킨 정사각형/left-right-inverted-square.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d", &n);
     for(int i = 0; i < n; i++){
         for(int j = n; j > 0; j--){
-            printf("%d ", n - j + 1 + i * n);
+            printf("%d ", (n - j + 1) * (i + 1));
         }printf("\n");
     }
     return 0;
