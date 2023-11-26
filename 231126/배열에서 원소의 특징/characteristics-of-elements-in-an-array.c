55490f9f14d4e91c80419a6b11e82a48b731ce30
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-26 19:00:49 +0900

    Title: 배열에서 원소의 특징, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231126/배열에서 원소의 특징/characteristics-of-elements-in-an-array.c
+++ 231126/배열에서 원소의 특징/characteristics-of-elements-in-an-array.c
@@ -2,11 +2,11 @@
 
 int main() {
     int arr[10];
-    for(int i = 1; i < 10; i++){
+    for(int i = 0; i < 10; i++){
         scanf("%d", &arr[i]);
     }
-    for(int i = 1; i < 10; i++){
-        if(i % 3 == 0){
+    for(int i = 0; i < 10; i++){
+        if(arr[i] % 3 == 0){
             printf("%d ", arr[i - 1]);
             break;
         }
