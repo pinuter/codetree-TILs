20797bc3a107c320f4cca97a22b3fd86805972c6
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-26 09:02:38 +0900

    Title: 학점 계산기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231126/학점 계산기/credit-calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    double arr[5];
+    double sum = 0;
+    for(int i = 0; i < n; i++){
+        scanf("%lf", &arr[i]);}
+
+        for(int i = 0; i < n; i++){
+        sum += arr[i];}
+        double avr;
+        avr = sum / n;
+        printf("%.1lf\n", avr);
+        if(avr >= 4.0){
+            printf("Perfect", avr);
+        }
+        else if(avr >= 3.0){
+            printf("Good", avr);
+        }
+        else{
+            printf("poor", avr);
+        }
+    
+    return 0;
+}
