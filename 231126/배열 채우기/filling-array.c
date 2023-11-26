624f59728dbdc271d6c89b8bf4644c3843d2f55e
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-26 09:36:19 +0900

    Title: 배열 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231126/배열 채우기/filling-array.c
+++ 231126/배열 채우기/filling-array.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    int arr[10];
-    scanf("%d", &arr[10]);
-    for(int i = 10; i > 0; i--){
-        printf("%d", i);
-        if(i == 0){
+    const int maxSize = 10;
+    int numbers[maxSize];
+    int input, count = 0;
+
+
+
+    // 최대 10개의 정수를 입력받음
+    for (int i = 0; i < maxSize; i++) {
+        scanf("%d", &input);
+
+        // 0이 입력되면 입력 중단
+        if (input == 0) {
             break;
         }
+
+        numbers[i] = input;
+        count++;
     }
+
+    // 가장 나중에 입력된 정수부터 출력
+    for (int i = count - 1; i >= 0; i--) {
+        printf("%d ", numbers[i]);
+    }
+
     return 0;
 }
