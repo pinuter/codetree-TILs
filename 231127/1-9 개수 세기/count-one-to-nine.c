ec9bf86993d029da45ed1a2aae057c76838e3237
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-27 13:14:26 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231127/1-9 개수 세기/count-one-to-nine.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    int count_arr[10] = {0};
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++){ 
+        count_arr[arr[i]]++;
+    }
+
+    for(int i = 1; i < 10; i++){
+        printf("%d\n", count_arr[i]);
+    }
+
+    return 0;
+}
