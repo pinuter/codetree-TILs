70451fbbd56cb482047b4af705a3ed381616b2dd
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-27 13:00:33 +0900

    Title: 숫자들의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231127/숫자들의 배수/multiple-of-numbers.c
+++ 231127/숫자들의 배수/multiple-of-numbers.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt = 0;
+    scanf("%d", &n);
+    int arr[100];
+    arr[0] = n;
+    for(int i = 1; i <= 10; i++){
+        arr[i]=n*(i);
+        printf("%d ", arr[i]);
+        if(arr[i] % 5 == 0){
+            cnt++;
+        }
+        if(cnt == 2){
+
+            break;
+        }
+    }
     return 0;
 }
