5e7a1eb3b684ae82044ab575f729644849587268
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-28 12:50:46 +0900

    Title: 특정 위치의 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231128/특정 위치의 문자/char-in-specific-location.c
+++ 231128/특정 위치의 문자/char-in-specific-location.c
@@ -6,15 +6,18 @@ int main() {
     int idx = -1;
 
     for(int i = 0; i < 6; i++){
-        if(arr[i] == 'L'){
+        if(arr[i] == 'L' || arr[i] == 'E' || arr[i] == 'B' || arr[i] == 'R' || arr[i] == 'O' || arr[i] == 'S'){
             idx = i;
+            break;  // 원하는 문자를 찾았으면 반복문 종료
         }
     }
+
     if(idx == -1){
         printf("None");
     }
     else{
         printf("%d", idx);
     }
+
     return 0;
 }
