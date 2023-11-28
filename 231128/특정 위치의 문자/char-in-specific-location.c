39b59235c816d8a9c762ea71fd5ef86c70e76da6
pinuter <151040717+pinuter@users.noreply.github.com>
2023-11-28 12:51:42 +0900

    Title: 특정 위치의 문자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231128/특정 위치의 문자/char-in-specific-location.c
+++ 231128/특정 위치의 문자/char-in-specific-location.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    char arr[6] = {'L', 'E', 'B', 'R', 'O', 'S'};
-    
-    int idx = -1;
-
-    for(int i = 0; i < 6; i++){
-        if(arr[i] == 'L' || arr[i] == 'E' || arr[i] == 'B' || arr[i] == 'R' || arr[i] == 'O' || arr[i] == 'S'){
-            idx = i;
-            break;  // 원하는 문자를 찾았으면 반복문 종료
-        }
-    }
-
-    if(idx == -1){
-        printf("None");
-    }
-    else{
-        printf("%d", idx);
-    }
+    // 배열을 만들고 문자 a를 입력받습니다.
+	char word[6] = { 'L', 'E', 'B', 'R', 'O', 'S' };
+	char a;
+	scanf("%c", &a);
+	// idx : a와 같은 문자의 인덱스, a와 같은 것이 없으면 -1
+	int idx = -1;
+	
+	// a와 같은 문자가 있는지 확인합니다.
+	for(int i = 0; i < 6; i++) {
+		if(word[i] == a)
+			idx = i;
+	}
+	
+	// 같은 것이 없으면 None을, 있다면 그 인덱스를 출력합니다.
+	if(idx == -1)
+		printf("None");
+	else
+		printf("%d", idx);
 
     return 0;
 }
