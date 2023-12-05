471ae6b520154e188009ed9273c8d7c8a40f9697
pinuter <151040717+pinuter@users.noreply.github.com>
2023-12-06 08:41:57 +0900

    Title: 가장 짧은 문자열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231206/가장 짧은 문자열/shortest-string.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    char arr[20];
+    char arr_2[20];
+    char arr_3[20];
+    scanf("%s %s %s", arr, arr_2, arr_3);
+
+    int len = 0, len_2 = 0, len_3 = 0;
+
+    for (int i = 0; i < 20; i++) {
+        if (arr[i] == '\0') {
+            len = i;
+            break;
+        }
+    }
+    for (int i = 0; i < 20; i++) {
+        if (arr_2[i] == '\0') {
+            len_2 = i;
+            break;
+        }
+    }
+    for (int i = 0; i < 20; i++) {
+        if (arr_3[i] == '\0') {
+            len_3 = i;
+            break;
+        }
+    }
+
+    if (len_2 > len && len_2 > len_3) {
+        printf("%d", len_2 - len);
+    } else if (len_3 > len && len_3 > len_2) {
+        printf("%d", len_3 - len);
+    } else if (len > len_2 && len > len_3) {
+        printf("%d", len - len_2);
+    } else {
+        printf("No difference");
+    }
+
+    return 0;
+}
