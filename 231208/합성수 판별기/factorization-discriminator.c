5d2e31d255b49fb474097907f64ea521025c38c6
pinuter <151040717+pinuter@users.noreply.github.com>
2023-12-08 13:23:31 +0900

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231208/합성수 판별기/factorization-discriminator.c
+++ 231208/합성수 판별기/factorization-discriminator.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    int n, v;
+    int n;
     scanf("%d", &n);
-    for(v = 2; n/v; v++) {
-        if(n/v == 1){
-            printf("C");
-            break;
-        }
-        else if(n/v == 0) {
-            printf("D");
+
+    // 2부터 n까지의 수로 나누어보며 약수를 확인
+    int isComposite = 0; // 합성수 여부를 나타내는 플래그
+
+    for (int v = 2; v <= n / 2; v++) {
+        if (n % v == 0) {
+            isComposite = 1; // 약수를 찾았으면 합성수로 플래그 설정
             break;
         }
     }
+
+    // 합성수인지 여부에 따라 출력
+    if (isComposite) {
+        printf("C"); // 합성수일 경우
+    } else {
+        printf("D"); // 합성수가 아닐 경우
+    }
+
     return 0;
 }
