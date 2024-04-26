c92a4c046999ba2729373229a0de2f23016419d9
pinuter <151040717+pinuter@users.noreply.github.com>
2024-04-26 09:22:40 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240426/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int exam;
+    cin >> exam;
+    if(exam == 100) {
+        cout << "pass";
+    }
+
+    else {
+        cout << "failure";
+    }
+    return 0;
+}
