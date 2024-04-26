476af02e9bdb5f7f40b86e06641b09b9efe3ada6
pinuter <151040717+pinuter@users.noreply.github.com>
2024-04-26 09:16:19 +0900

    Title: 비교에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240426/비교에 따른 연산/operation-based-on-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, d;
+    cin >> a >> b;
+    c = a * b;
+    d = b/a;
+
+    if(a > b) {
+        cout << c;
+    }
+    else {
+        cout << d;
+    }
+    return 0;
+}
